fc24b0f91fd89825fd5243755c1a71008ba786c8
Newt0n <47007924+Blackcloud00@users.noreply.github.com>
2025-11-22 22:28:55 +0300

    Enemy's Respawn added
    Enemy's Respawn added

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private Transform respawn;
+    [SerializeField] private float spawnCooldown;
+    private float spawnTimer;
+
+
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject basicEnemy;
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if(spawnTimer <= 0)
+        {
+            CreateEnemy();
+            spawnTimer = spawnCooldown;
+        }
+    }
+
+    private void CreateEnemy()
+    {
+        GameObject newEnemy = Instantiate(basicEnemy, respawn.position, Quaternion.identity);
+    }
+}
