20a7d09abaab07fdcc3197802f5275c6f5fd2508
Newt0n <47007924+Blackcloud00@users.noreply.github.com>
2025-12-07 14:08:03 +0300

    Moving Rotor during set period of time

--- Assets/Scripts/Crossbow_Visuals.cs
+++ Assets/Scripts/Crossbow_Visuals.cs
@@ -18,27 +18,79 @@ public class Crossbow_Visuals : MonoBehaviour
     [SerializeField] private float maxIntensity = 150;
     [Space]
     [SerializeField] private Color startColor;
+
+    [Header("Rotor Visuals")]
+    [SerializeField] private Transform rotor;
+    [SerializeField] private Transform rotorUnloaded;
+    [SerializeField] private Transform rotorLoaded;
+
+
     [SerializeField] private Color endColor;
+
+    [Header("Front Glow String")]
+    [SerializeField] private LineRenderer frontString_L;
+    [SerializeField] private LineRenderer frontString_R;
+
+    [Space]
+
+    [SerializeField] private Transform frontStartPoint_L;
+    [SerializeField] private Transform frontStartPoint_R;
+    [SerializeField] private Transform frontEndPoint_R;
+    [SerializeField] private Transform frontEndPoint_L;
+
+    [Header("Back Glow String")]
+    [SerializeField] private LineRenderer backString_L;
+    [SerializeField] private LineRenderer backString_R;
+        
+    [Space]
+
+    [SerializeField] private Transform backStartPoint_L;
+    [SerializeField] private Transform backStartPoint_R;
+    [SerializeField] private Transform backEndPoint_R;
+    [SerializeField] private Transform backEndPoint_L;
+
+    [SerializeField] private LineRenderer[] lineRenderers;
     private void Awake()
     {
         myTower = GetComponent<Tower_Crossbow>();
-    
         material = new Material(meshRenderer.material);
-
         meshRenderer.material = material;
 
+        UpdateMaterialsOnLineRenderers();
+
         StartCoroutine(ChangeEmission(1));
+    }
+
+    private void UpdateMaterialsOnLineRenderers()
+    {
+        foreach (var lr in lineRenderers)
+        {
+            lr.material = material;
+        }
     }
 
     private void Update()
     {
         UpdateEmissionColor();
+        UpdateStrings();
+
+    }
+
+    private void UpdateStrings()
+    {
+        UpdateStringVisual(frontString_R, frontStartPoint_R, frontEndPoint_R);
+        UpdateStringVisual(frontString_L, frontStartPoint_L, frontEndPoint_L);
+        UpdateStringVisual(backString_L, backStartPoint_L, backEndPoint_L);
+        UpdateStringVisual(backString_R, backStartPoint_R, backEndPoint_R);
     }
 
     public void PlayReloadFX(float duration)
     {
-        StartCoroutine(ChangeEmission(duration / 2));
-    }
+        float newDuration = duration / 2;
+
+        StartCoroutine(ChangeEmission(newDuration));
+        StartCoroutine(UpdateRotorDuration(newDuration));
+    }   
     private void UpdateEmissionColor()
     {
         Color emissionColor = Color.Lerp(startColor, endColor, currentIntensity / maxIntensity);
@@ -83,5 +135,25 @@ public class Crossbow_Visuals : MonoBehaviour
         }
 
         currentIntensity = maxIntensity;
+    }
+
+    IEnumerator UpdateRotorDuration(float duration)
+    {
+        float startTime = Time.time;
+
+        while(Time.time - startTime < duration)
+        {
+            float tValue = (Time.time - startTime) / duration;
+            rotor.position = Vector3.Lerp(rotorUnloaded.position, rotorLoaded.position, tValue);
+            yield return null;
+        }
+
+        rotor.position = rotorLoaded.position;  
+    }
+
+    private void UpdateStringVisual(LineRenderer lineRenderer, Transform startPoint, Transform endPoint)
+    {
+        lineRenderer.SetPosition(0, startPoint.position);
+        lineRenderer.SetPosition(1, endPoint.position);
     }
 }
